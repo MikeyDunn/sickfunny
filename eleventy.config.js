92a79260259cb63f6de7396dd735ebbef0718c25
Mike Dunn <mikey.dunn@yahoo.com>
2025-05-19 19:53:35 -0500

    version css file

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,21 @@
+import path from 'path'
+import crypto from 'crypto'
+import fs from 'fs'
+
 import { minify } from 'html-minifier'
 import rssPlugin from '@11ty/eleventy-plugin-rss'
 import eleventyImage from '@11ty/eleventy-img'
 
 export default function (eleventyConfig) {
+  // Plugins
   eleventyConfig.addPlugin(rssPlugin)
+
+  // Passthrough
   eleventyConfig.addPassthroughCopy('img')
-  eleventyConfig.addPassthroughCopy({ 'src/css': 'css' })
   eleventyConfig.addPassthroughCopy('src/favicon.png')
 
-  eleventyConfig.addFilter('date', function (value, format = 'en-US') {
-    return new Date(value).toLocaleDateString(format, {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    })
-  })
-
-  eleventyConfig.addNunjucksAsyncShortcode('image', async (src, alt) => {
+  // Shortcodes
+  eleventyConfig.addNunjucksAsyncShortcode('image', async (src, alt, options = {}) => {
     let metadata = await eleventyImage(src, {
       widths: [320, 640, 1024],
       formats: ['avif', 'webp', 'jpeg'],
@@ -28,15 +27,18 @@ export default function (eleventyConfig) {
       alt,
       loading: 'lazy',
       decoding: 'async',
+      ...options,
     }
 
     return eleventyImage.generateHTML(metadata, imageAttributes)
   })
 
+  // Collections
   eleventyConfig.addCollection('post', function (collectionApi) {
     return collectionApi.getFilteredByGlob('./src/posts/**/*.md')
   })
 
+  // Minify HTML
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (outputPath && outputPath.endsWith('.html')) {
       return minify(content, {
@@ -46,6 +48,38 @@ export default function (eleventyConfig) {
       })
     }
     return content
+  })
+
+  // Hash CSS files
+  const outputMap = {}
+  eleventyConfig.on('eleventy.before', () => {
+    const inputPath = 'src/css/styles.css'
+    const outputDir = '_site/css/'
+
+    const css = fs.readFileSync(inputPath)
+    const hash = crypto.createHash('md5').update(css).digest('hex').slice(0, 8)
+    const outputFilename = `styles.${hash}.css`
+
+    fs.mkdirSync(outputDir, { recursive: true })
+    fs.writeFileSync(path.join(outputDir, outputFilename), css)
+
+    outputMap[inputPath] = `/css/${outputFilename}`
+  })
+
+  // Filters
+  eleventyConfig.addFilter('date', function (value, format = 'en-US') {
+    return new Date(value).toLocaleDateString(format, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    })
+  })
+
+  eleventyConfig.addFilter('hashedCss', function (filepath) {
+    if (!outputMap[filepath]) {
+      throw new Error(`hashedCss: ${filepath} not found`)
+    }
+    return outputMap[filepath]
   })
 
   return {
